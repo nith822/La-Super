4e38b0a2e540e157a9bee42f75638836309ea17f
houkagoteatime <1harveygao@gmail.com>
2019-12-18 13:34:40 -0600

    Add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FrontPageComponent } from './main/components/pages/front-page/front-page.component';
+import { PrimaryProductPageComponent } from './main/components/pages/primary-product-page/primary-product-page.component';
 
 
-const routes: Routes = [ { path: '', component: FrontPageComponent } ];
+const routes: Routes = [ { path: '', component: FrontPageComponent },
+						 { path: 'products', component: PrimaryProductPageComponent } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
