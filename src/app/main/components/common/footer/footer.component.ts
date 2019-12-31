959be72fee61b46f0e327f47146fc170cc1b3988
houkagoteatime <1harveygao@gmail.com>
2019-12-31 00:35:00 -0600

    Add basic footer

--- src/app/main/components/common/footer/footer.component.ts
+++ src/app/main/components/common/footer/footer.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['../header/header.component.css', './footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	urls: string[] = ['contact'];
+	public location: string = "1201 E Parker Rd #101, Plano, TX 75074"
+	public phone_number: string = "(972)422-2592";
+	
+	public sample_text: string = "The brown fox jumped over the lazy dog."
+	public sample_hours: string = "10-10:30";
+	
+	constructor(private router: Router) { }
+	
+	navigateTo(option: number): void {
+		this.router.navigateByUrl(this.urls[option]);
+	}
+	
+	ngOnInit() {
+	}
 
 }
